4610e53b2821251415b3d8eab51d174b137a40c7
Daryll Santos <daryll.santos@gmail.com>
2018-04-09 21:08:48 +0800

    Removed lit, added react-bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
-import '@ajusa/lit/dist/lit.css';
-import '@ajusa/lit/dist/util.css';
 import './index.css';
 import './App.css';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import Home from './containers/home'
 import Checklist from './containers/checklist'
 import Parking from './containers/parking'
@@ -13,14 +12,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <div className="c row">
-            <Link className="ph2" to="/">Home</Link>
-            <Link className="ph2" to="/checklist">Checklist</Link>
-            <Link className="ph2" to="/parking">Parking</Link>
-            <Link className="ph2" to="/shopping">Shopping</Link>
-          </div>
-        </header>
+        <Navbar inverse>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link className="ph2" to="/">Bike Paps</Link>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem href="#">
+              <Link className="ph2" to="/">Home</Link>
+            </NavItem>
+            <NavItem href="#">
+              <Link className="ph2" to="/checklist">Checklist</Link>
+            </NavItem>
+            <NavItem href="#">
+              <Link className="ph2" to="/parking">Parking</Link>
+            </NavItem>
+            <NavItem href="#">
+              <Link className="ph2" to="/shopping">Shopping</Link>
+            </NavItem>
+          </Nav>
+        </Navbar>
         <Route exact path="/" component={Home} />
         <Route exact path="/checklist" component={Checklist} />
         <Route exact path="/parking" component={Parking} />
